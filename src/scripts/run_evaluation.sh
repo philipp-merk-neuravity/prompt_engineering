0661c7dd914ace04a9916eef3b5f34b8aa106a4f
philipp <merk.philipp.user@gmail.com>
2024-03-11 19:54:43 +0100

    add for 0.8

--- src/scripts/run_evaluation.sh
+++ src/scripts/run_evaluation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 PYTHON_SCRIPT="/home/neuravity/dev/prompt_engineering/src/human_eval/human_eval/evaluate_functional_correctness.py"
-BENCHMARK_RESULTS="/home/neuravity/dev/prompt_engineering/src/benchmark_results/all/simple/0.8/zero_shot_cot/gpt-4-0125-preview/1/1.jsonl"
+BENCHMARK_RESULTS="/home/neuravity/dev/prompt_engineering/src/benchmark_results/all/simple/0.8/scot/gpt-4-0125-preview/0/0.jsonl"
 PROBLEM_FILE="/home/neuravity/dev/prompt_engineering/src/human_eval/data/HumanEval.jsonl"
 PYTHON_INTERPRETER_PATH="/home/neuravity/dev/prompt_engineering/venv/bin/activate"
 
 source "$PYTHON_INTERPRETER_PATH"
-python3 "$PYTHON_SCRIPT" "$BENCHMARK_RESULTS" --problem_file="$PROBLEM_FILE" --k="1"
+python3 "$PYTHON_SCRIPT" "$BENCHMARK_RESULTS" --problem_file="$PROBLEM_FILE" --k="5"
 deactivate
